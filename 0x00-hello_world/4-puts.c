9039c91cbc4a9d743cc0f6517b3b5e88a9632862
Odarell35 <leratomawash0@gmail.com>
2023-02-16 12:48:33 -0800

    printf function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main entry
+ * main - entry a C program that prints the following string
  * Return:Always 0(Success)
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle/n");
+	puts("\"Programming is like building a multilingual puzzle/n");
 	return (0);
 }
