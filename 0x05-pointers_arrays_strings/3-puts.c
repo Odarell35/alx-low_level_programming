bf53b0a8c87240a41c8c8d0e00f8b754142b36fc
Odarell35 <leratomawash0@gmail.com>
2023-02-27 11:41:23 -0800

    print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line.
+ * @str: input string.
+ * Return: no return.
+ */
+
+void _puts(char *str)
+{
+	int num;
+
+	for (num = 0; str[num] != '\0'; num++)
+	{ 
+		_putchar(str[num]);
+	}
+	_putchar('\n');
+}
