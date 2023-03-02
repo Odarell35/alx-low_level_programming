419ab0ec58fdc3ac0d71d27903eeae5941f22add
Odarell35 <leratomawash0@gmail.com>
2023-03-02 05:01:47 -0800

    string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,18 +7,23 @@
 
 char *cap_string(char *s)
 {
-	int i = 0;
+	int i;
+	int j;
+	char separators[] = {9, 10, 32, 34, 40, 41, 44, 46, 59, 63, 123, 125};
 
-	if (s[i] >= 97 && s[i] <= 122)
-		s[i] = s[i] - 32;
-	i++;
-	for (i = 1; s[i] != '\0'; i++)
+	i = 0;
+	while(s[i] != '\0')
 	{
+		if (s[i] >= 97 && s[i] <= 122)
+			s[i] = s[i] - 32;
+			break;
+				for (j = 0; j < 13; j++)
+				{
+					if (s[i] == separators[j])
+						s[i] = s[i + 1] - 32;
+				}
 
-if (s[i] == 9 || s[i] == 10 || s[i] == 32 || s[i] == 34 || s[i] == 40 || s[i] == 41 || s[i] == 44 || s[i] == 46 || s[i] == 59 || s[i] == 63 || s[i] == 123 || s[i] == 125)
-			s[i + 1] = s[i + 1] - 32;
-		break;
-
+	i++;
 	}
 return (s);
 }
