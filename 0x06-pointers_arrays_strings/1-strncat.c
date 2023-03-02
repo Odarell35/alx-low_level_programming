8267e67ef93a5eef2f76c579853ad59364070166
Odarell35 <leratomawash0@gmail.com>
2023-03-02 04:26:46 -0800

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -25,8 +25,10 @@ char *_strncat(char *dest, char *src, int n)
 	for (src_index = 0; src_index < n && src[src_index] != '\0'; src_index++)
 	{
 		dest[length_dest + src_index] = src[src_index];
-		dest[length_dest + src_index] = '\0';
+		if (dest[length_dest + src_index] == '\0')
+		{
 		break;
+		}
 	}
 return (dest);
 }
