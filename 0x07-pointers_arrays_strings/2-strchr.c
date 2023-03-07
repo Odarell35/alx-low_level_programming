f6a3375141e614d0e461f3eec23132c8bfcd3b94
Odarell35 <leratomawash0@gmail.com>
2023-03-07 00:43:59 -0800

    fixing

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,16 +9,7 @@
 char *_strchr(char *s, char c)
 {
 	int i;
-	/*int length_string;
-	int l;
-	
-	length_string = 0;
-	l = 0;
-	while (s[l] != '\0')
-	{
-		length_string++;
-			l++;
-	}*/
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
