5b3d8d7a408a34b2a8eb8a9f7597fef6f0e99f18
Odarell35 <leratomawash0@gmail.com>
2023-03-08 04:33:28 -0800

    factorial works

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -6,13 +6,10 @@
  */
 int factorial(int n)
 {
-	int f = 0;
-
 	if (n < 0)
 		return (-1);
 	else if (n == 0)
 		return (1);
 	else
-		f = n * factorial(n - 1);
-		return (f);
+		return (n * factorial(n - 1));
 }
