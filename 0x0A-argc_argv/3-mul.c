8b245f8ddc3c9c6975752244ab8b80c0795f5c33
Odarell35 <leratomawash0@gmail.com>
2023-03-10 14:16:13 -0800

    fixing

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,13 +9,14 @@
 int main(int argc, char *argv[])
 {
 	int mul;
+
 	if (argc == 3)
 	{
 		mul = atoi(argv[1]) * atoi(argv[2]);
-        	printf("%d\n",mul);
+		printf("%d\n", mul);
 		return (0);
 	}
-		else 
+		else
 		printf("Error\n");
 return (1);
 }
