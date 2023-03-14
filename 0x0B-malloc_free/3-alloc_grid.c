80d89553442154cb17f4c026cb3a6d8fc1f119ac
Odarell35 <leratomawash0@gmail.com>
2023-03-14 00:08:26 -0700

    fixing

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,7 +14,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	grid = malloc(sizeof(int) * width);
+	grid = malloc(sizeof(int *) * width);
 	if (grid == NULL)
 	{
 		free(grid);
@@ -22,7 +22,7 @@ int **alloc_grid(int width, int height)
 	}
 	for (i = 0; i < width; i++)
 	{
-		grid[i] = malloc(sizeof(int) * height);
+		grid[i] = malloc(sizeof(int *) * height);
 			if (grid == NULL)
 		{
 			free(grid);
