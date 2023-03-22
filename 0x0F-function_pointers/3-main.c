67dcf333b0e992672d181e3adfc661c7a07c09a8
Odarell35 <leratomawash0@gmail.com>
2023-03-22 06:09:59 -0700

    fixing task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -16,6 +16,11 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 			exit(98);
 	}
+	if (argv[2][1])
+	{
+		printf("Error\n");
+		exit(99);
+	}
 	calculation = get_op_func(argv[2]);
 	if (calculation == NULL)
 	{
