7775e54889c666a57c98a20dd21eac884f8e5484
Oradell35 <122868692+Odarell35@users.noreply.github.com>
2023-03-30 19:58:27 +0200

    Update 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -34,7 +34,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	{
 		while (temp->next != 0)
 			temp = temp->next;
-		temp->next = new_node;
+			temp->next = new_node;
 	}
 
 	return (*head);
