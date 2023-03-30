bb758b8502abd68aadd92b58a16c69060352ba09
Odarell35 <leratomawash0@gmail.com>
2023-03-30 07:52:15 -0700

    fixing the file

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,13 +14,13 @@ size_t print_list(const list_t *h)
 
 	if (h->str == NULL)
 	{
-		printf("[0] (nill)");
+		printf("[%d] %s\n", 0, "(nil)");
 	}
 	else
 	{
 		while (h != 0)
 	{
-		printf("[%d]%s\n", h->len, h->str);
+		printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		count++;
 	}
