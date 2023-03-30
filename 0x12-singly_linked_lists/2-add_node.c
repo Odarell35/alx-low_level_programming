3c1c0755592051c55ee2b5c43bb61a007738c6bc
Odarell35 <leratomawash0@gmail.com>
2023-03-30 15:32:32 -0700

    add node runs!

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,11 +11,15 @@
 list_t *add_node(list_t **head, const char *str)
 {
 list_t *new_node;
+size_t number_char;
 
 new_node = malloc(sizeof(list_t));
 if (new_node == NULL)
 return (NULL);
 new_node->str = strdup(str);
+for (number_char = 0; str[number_char]; number_char++)
+	;
+new_node->len = number_char;
 new_node->next = *head;
 *head = new_node;
 return (*head);
