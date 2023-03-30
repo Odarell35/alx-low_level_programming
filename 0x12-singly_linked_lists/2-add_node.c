b54b145355f7756f3c93cb670a13675204b9d4e0
Oradell35 <122868692+Odarell35@users.noreply.github.com>
2023-03-30 19:47:53 +0200

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -8,7 +8,7 @@
  * @str: string to store in the list.
  * Return: address of the head.
  */
-list_t *add_node_end(list_t **head, const char *str)
+list_t *add_node(list_t **head, const char *str)
 {
 list_t *new_node;
 
