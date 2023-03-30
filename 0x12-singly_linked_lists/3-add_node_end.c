5ce74d002609377f837d05c576cf18cb7c5e00b8
Odarell35 <leratomawash0@gmail.com>
2023-03-30 15:49:44 -0700

    adding node at end works

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,5 +1,5 @@
 #include "lists.h"
-
+#include <string.h>
 /**
  * add_node_end - adds a new node at the end
  * of a list_t list.
@@ -33,8 +33,10 @@ list_t *add_node_end(list_t **head, const char *str)
 	else
 	{
 		while (temp->next != 0)
+		{
 			temp = temp->next;
 			temp->next = new_node;
+		}
 	}
 
 	return (*head);
