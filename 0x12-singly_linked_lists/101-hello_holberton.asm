278d935d3ed0cafafd3651f10c053499df63fa25
Odarell35 <leratomawash0@gmail.com>
2023-03-30 17:20:16 -0700

    fixing

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,19 @@
 section .data
-    message db "Hello, Holberton", 0Ah ; 0Ah is the ASCII code for a new line character
-    format db "%s", 10, 0
+    message db 'Hello, Holberton', 0Ah ; message to be printed
+
 section .text
-extern printf
-global _start
+    global _start
 
 _start:
-    ; print the message
-    mov esi, message
-	mov edi, format
-	mov eax, 0
-	call printf
+    ; write message to stdout
+    mov eax, 4      ; syscall write
+    mov ebx, 1      ; file descriptor stdout
+    mov ecx, message    ; message to be printed
+    mov edx, 16     ; message length
+    int 0x80        ; call kernel
 
-	mov eax, 0
-	ret
+    ; exit program
+    mov eax, 1      ; syscall exit
+    xor ebx, ebx    ; return code 0
+    int 0x80        ; call kernel
+
