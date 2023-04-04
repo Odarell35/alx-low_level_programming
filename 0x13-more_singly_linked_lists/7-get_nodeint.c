6bb3f549b59c0620351ddb534e9635312d03a393
Odarell35 <leratomawash0@gmail.com>
2023-04-04 01:55:39 -0700

    can get the nth node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -19,7 +19,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	{
 		actual_node = actual_node->next;
 	}
-	if (i == index)
+	if (i == index && actual_node != NULL)
 	{
 		return (actual_node);
 	}
