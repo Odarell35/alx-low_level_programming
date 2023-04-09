784657ef9a87e4266e73fcc878cf22064e563cd2
Oradell35 <122868692+Odarell35@users.noreply.github.com>
2023-04-09 16:03:47 +0200

    update

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,8 +2,13 @@
 #define _MAIN_H
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include<unistd.h>
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _putchar(char c);
+size_t __cdecl fwrite(const void * __restrict__ _Str,size_t _Size,size_t _Count,FILE * __restrict__ _File);
 #endif
