f9cbf9b3baba8eefaa91ad6d8f222928fad4fd32
Odarell35 <leratomawash0@gmail.com>
2023-05-08 12:36:11 -0700

    fixing header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -11,5 +11,4 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _putchar(char c);
-size_t __cdecl fwrite(const void * __restrict__ _Str,size_t _Size,size_t _Count,FILE * __restrict__ _File);
 #endif
