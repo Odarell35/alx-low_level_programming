ee7ce30df25708021ed5948553fd2e94ddbe0de8
Odarell35 <leratomawash0@gmail.com>
2023-05-08 13:36:32 -0700

    fixed read text file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,41 +1,43 @@
 #include "main.h"
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output.
+ * read_textfile - reads a text file and prints
+ * it to the POSIX standard output.
  * @filename: pointer to file
  * @letters: number of letters it should read and print
+ * Return: number of lettere read and printed
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 char *text;
-FILE *filename_pointer;
+ssize_t open_file;
 ssize_t number_letters;
 ssize_t read_result;
+
 number_letters = 0;
-    
-filename_pointer = fopen("filename", "r");
+open_file = fopen("filename", O_RDONLY);
 text = malloc(sizeof(char) * (letters + 1));
-    
+
 if (filename == NULL)
 {
-return 0;
+return (0);
 }
 
 if (filename_pointer == NULL)
 {
-return 0;
+return (0);
 }
-if (text == NULL) 
+if (text == NULL)
 {
-fclose(filename_pointer);
-return 0;
+close(open_file);
+return (0);
 }
-while ((read_result = fread(text, 1, letters, filename_pointer)) > 0)
-{   
+while ((read_result = read(open_file, text, letters) > 0))
+{
 write(STDOUT_FILENO, text, read_result);
 number_letters = number_letters + read_result;
 }
 free(text);
-fclose(filename_pointer);
+close(open_file);
 
-return(number_letters);
+return (number_letters);
 }
