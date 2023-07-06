ed71b4e08eea415c64bce240ad7adf5111508659
Odarell35 <leratomawash0@gmail.com>
2023-07-06 03:43:01 -0700

    created empty hashtable

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,3 +1,9 @@
+#ifndef _HASH_TABLES_H
+#define _HASH_TABLES_H
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -26,3 +32,6 @@ typedef struct hash_table_s
      unsigned long int size;
      hash_node_t **array;
 } hash_table_t;
+
+hash_table_t *hash_table_create(unsigned long int size);
+#endif
